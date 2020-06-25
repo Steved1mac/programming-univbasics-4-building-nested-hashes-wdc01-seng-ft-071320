b979c0b59d96ff34efa411833069a8d8c41fcdce
Steven McAllister <steved1mac@gmail.com>
2020-06-25 22:50:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ def monopoly_with_third_tier
          :reading_railroad=>{},
           :pennsylvania_railroad=>{},
            :b_and_o_railroad=>{},
-            :shortline_railroad=>{}}}}}
+            :shortline_railroad=>{}}}}
 end
 
 def monopoly_with_fourth_tier
